9ee9a557c34a08883d89da193e17d3a05ab6b974
Eugene <eugeneayonga@gmail.com>
2023-05-15 15:33:48 +0300

    Functions illustrated, hot reload illustrated

--- lib/main.dart
+++ lib/main.dart
@@ -1,5 +1,7 @@
 import 'package:flutter/material.dart';
 
+String getFullName(String firstName, String lastName) => '$firstName $lastName';
+
 void main() {
   runApp(const MyApp());
 }
@@ -10,6 +12,7 @@ class MyApp extends StatelessWidget {
   // This widget is the root of your application.
   @override
   Widget build(BuildContext context) {
+    print(getFullName('John', 'Doe'));
     return MaterialApp(
       title: 'Flutter Demo',
       theme: ThemeData(
